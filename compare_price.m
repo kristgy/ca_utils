374aaa24962522e165ee51f733d98a63b7bd4e2b
kristgy <kristgy@users.noreply.github.com>
2020-11-14 23:32:09 +0100

    First commit.

---
+++ compare_price.m
@@ -0,0 +1,20 @@
+clear all
+close all
+
+sub = struct('name',{{'Enkeltariff E4', 'Tidstariff T4'}},...
+'low_price',[.32 .18],'high_price',[.32 .65],...
+'high_month',[1 2 3 11 12], 'high_day', [2:6], 'high_hour',[6:21])
+%'high_month',[1 2 3 7 8 9 10 11 12], 'high_day', [2:6], 'high_hour',[6:21])
+
+load('processed_usage')
+
+consum_high = sum(cons_acc(:,sub.high_month,sub.high_day,sub.high_hour,:),'all')
+consum_total = sum(cons_acc,'all')
+consum_low = consum_total - consum_high
+
+for s = 1:length(sub.name)
+	cost_high = consum_high*sub.high_price(s)
+	cost_low = consum_low*sub.low_price(s)
+	cost_total = cost_high + cost_low
+end
+
