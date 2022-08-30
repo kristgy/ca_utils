cc82e048e5633cabf7e87e74b624b7fe01c207ea
kristgy <kristgy@users.noreply.github.com>
2022-08-30 23:24:00 +0200

    Fixed off-by one error in definition of tidstariff.

--- code/analyse_time_tariff_transm.m
+++ code/analyse_time_tariff_transm.m
@@ -1,16 +1,20 @@
 clear all
 close all
 
+run conf.m
+
 % For prices see:
 % https://www.vattenfalleldistribution.se/el-hem-till-dig/elnatspriser/
 % Prices include VAT
 
 sub = struct('name',{{'Enkeltariff E4', 'Tidstariff T4'}},...
 'low_price',[.305 .18],'high_price',[.305 .60],...
-'high_month',[1 2 3 11 12], 'high_day', [2:6], 'high_hour',[6:21])
+'high_month',[1 2 3 11 12], 'high_day', [2:6], 'high_hour',[7:22])
+% NB! Careful with off by one error in high_hour due to Matlab indexing from 1.
 %'high_month',[1 2 3 7 8 9 10 11 12], 'high_day', [2:6], 'high_hour',[6:21])
 
-load('processed_usage')
+%load('processed_usage')
+load([tmp_data_dir cons_file])
 
 consum_high = sum(cons_acc(:,:,sub.high_month,sub.high_day,sub.high_hour,:),'all')
 consum_total = sum(cons_acc,'all')
