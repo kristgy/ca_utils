13f65f5f769463e7f1110675796c26a573789eab
kristgy <kristgy@users.noreply.github.com>
2022-11-05 20:26:44 +0100

    Code cleanup.

--- code/process_cons_data.m
+++ code/process_cons_data.m
@@ -19,38 +19,36 @@ T = readtable([cons_data_dir data_file],opts);
 %T.StartTime = date_fun(T.StartTime);
 %T.EndTime = date_fun(T.EndTime);
 T.ConsumptionKWh = decimal_fun(T.ConsumptionKWh);
-TT = table2timetable(T,'RowTimes','StartTime');
+%TT = table2timetable(T,'RowTimes','StartTime');
 dur = T.EndTime-T.StartTime;
 
-charger_name = unique(TT.ChargerName);
-first_name = unique(TT.FirstName);
-email = unique(TT.Email);
-last_name = unique(TT.LastName);
-%num_users = length(first_name);
+charger_name = unique(T.ChargerName);
+first_name = unique(T.FirstName);
+email = unique(T.Email);
+last_name = unique(T.LastName);
 num_users = length(email);
-%us_lut = cell2struct(num2cell(1:num_users),matlab.lang.makeValidName(first_name),2);
 us_lut = cell2struct(num2cell(1:num_users),matlab.lang.makeValidName(email),2);
-users = struct()
+users = struct();
 
-cons_years = year(min(TT.StartTime)):year(max(TT.StartTime));
-%cons_acc = zeros(num_users,12,7,24,60);
+cons_years = year(min(T.StartTime)):year(max(T.EndTime));
+% FIXME Initialize cons_acc also with NaN?
 cons_acc = zeros(num_users,length(cons_years),12,7,24,60);
 cons_full = NaN*ones(num_users,length(cons_years),12,31,24,60);
 
 %return
 
 for s = 1:length(dur)
-	Consumption = ones(floor(minutes(dur(s))),1)*TT.ConsumptionKWh(s)/floor(minutes(dur(s)));
+	Consumption = ones(floor(minutes(dur(s))),1)*T.ConsumptionKWh(s)/floor(minutes(dur(s)));
 	%User = matlab.lang.makeValidName(TT.FirstName(s));
-	User = matlab.lang.makeValidName(TT.Email(s));
-	users.ChargerName(us_lut.(User{:})) = TT.ChargerName(s);
-	users.FirstName(us_lut.(User{:})) = TT.FirstName(s);
-	users.LastName(us_lut.(User{:})) = TT.LastName(s);
-	users.Email(us_lut.(User{:})) = TT.Email(s);
+	User = matlab.lang.makeValidName(T.Email(s));
+	users.ChargerName(us_lut.(User{:})) = T.ChargerName(s);
+	users.FirstName(us_lut.(User{:})) = T.FirstName(s);
+	users.LastName(us_lut.(User{:})) = T.LastName(s);
+	users.Email(us_lut.(User{:})) = T.Email(s);
 	if length(User{:}) == 0 
 		User = {'NN'};
 	end
-	s_tab = timetable(Consumption,'TimeStep',minutes(1),'StartTime',TT.StartTime(s));
+	s_tab = timetable(Consumption,'TimeStep',minutes(1),'StartTime',T.StartTime(s));
 	for m = 1:length(Consumption)
 		%cons_acc(us_lut.(User{:}),month(s_tab.Time(m)),weekday(s_tab.Time(m)),hour(s_tab.Time(m))+1,minute(s_tab.Time(m))+1) = ...
 		%cons_acc(us_lut.(User{:}),month(s_tab.Time(m)),weekday(s_tab.Time(m)),hour(s_tab.Time(m))+1,minute(s_tab.Time(m))+1) + s_tab.Consumption(m);
@@ -62,4 +60,4 @@ for s = 1:length(dur)
 	end
 end
 
-save([tmp_data_dir cons_file],'TT','first_name','cons_acc','cons_full','cons_years')
+save([tmp_data_dir cons_file],'T','first_name','cons_acc','cons_full','cons_years')
