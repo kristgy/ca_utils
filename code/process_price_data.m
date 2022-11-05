bd0d89faafff36e8f687bed77d555e56733e79aa
kristgy <kristgy@users.noreply.github.com>
2022-11-05 21:01:02 +0100

    Code cleanup.

--- code/process_price_data.m
+++ code/process_price_data.m
@@ -8,12 +8,14 @@ run conf.m
 
 date_fun = @(date_str) datetime(date_str,'InputFormat','yyyy-MM-dd HH:mm','TimeZone','Europe/Zurich');
 
-datafiles = dir([price_data_dir price_data_file_str])
+price = struct();
+
+datafiles = dir([cf.price_data_dir cf.price_data_file_str])
 
 Prices = timetable();
 
 for file = 1:length(datafiles)
-	T = readtable([price_data_dir datafiles(file).name]);
+	T = readtable([cf.price_data_dir datafiles(file).name]);
 	T.Properties.VariableNames = ["Time","Price"];
 	T.Time = date_fun(T.Time);
 	TT = table2timetable(T,'RowTimes','Time');
@@ -22,13 +24,13 @@ end
 
 Prices = sortrows(Prices,'Time');
 
-price_years = year(min(Prices.Time)):year(max(Prices.Time));
-pr = zeros(length(price_years),12,7,24);
-pr_full = NaN*ones(length(price_years),12,31,24);
+price.years = year(min(Prices.Time)):year(max(Prices.Time));
+price.day_of_week = zeros(length(price.years),12,7,24);
+price.day_of_month = NaN*ones(length(price.years),12,31,24);
 
 for h = 1:length(Prices.Time)
-	pr(year(Prices.Time(h))-price_years(1)+1,month(Prices.Time(h)),weekday(Prices.Time(h)),hour(Prices.Time(h))+1) = Prices.Price(h);
-	pr_full(year(Prices.Time(h))-price_years(1)+1,month(Prices.Time(h)),day(Prices.Time(h)),hour(Prices.Time(h))+1) = Prices.Price(h);
+	price.day_of_week(year(Prices.Time(h))-price.years(1)+1,month(Prices.Time(h)),weekday(Prices.Time(h)),hour(Prices.Time(h))+1) = Prices.Price(h);
+	price.day_of_month(year(Prices.Time(h))-price.years(1)+1,month(Prices.Time(h)),day(Prices.Time(h)),hour(Prices.Time(h))+1) = Prices.Price(h);
 end
 
-save([tmp_data_dir price_file],'Prices','pr','pr_full','price_years') 
+save([cf.tmp_data_dir cf.price_file],'price') 
