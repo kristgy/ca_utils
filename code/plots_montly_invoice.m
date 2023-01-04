7b50c74f9ee93b350c95e2af04a0809145c5953f
kristgy <kristgy@users.noreply.github.com>
2023-01-04 17:09:55 +0000

    Refactoring.

--- code/plots_montly_invoice.m
+++ code/plots_montly_invoice.m
@@ -3,13 +3,6 @@ close all
 
 run conf.m
 
-%sel_m = [10];
-%sel_m = [11];
-sel_m = [12];
-%sel_y = [2];
-sel_y = [3];
-sel_u = [4];
-
 papersize = [18 8];
 offs_x = -.9;
 offs_y = -.7;
@@ -17,29 +10,30 @@ printfigs = 1;
 %printfigs = 0;
 
 load([cf.tmp_data_dir cf.cons_file],'cons');
+c_y_idx = find(cons.years==cf.yr);
 load([cf.tmp_data_dir cf.price_file],'price')
+p_y_idx = find(price.years==cf.yr);
 
-num_days_mon = datetime(price.years(sel_y),sel_m+1,0).Day;
+num_days_mon = datetime(price.years(p_y_idx),cf.m+1,0).Day;
 hours = 1:24*31;
 days = 1:31;
-start_weekday_mon = weekday(sprintf('%d-%d-01',price.years(sel_y),sel_m));
+start_weekday_mon = weekday(sprintf('%d-%d-01',price.years(p_y_idx),cf.m));
 weekdays = mod(start_weekday_mon+days-2,7) + 1;
 
 cons_hour = squeeze(sum(cons.day_of_month,6,'omitnan'));
 if cf.hourly_prices
-	mon_price = transpose(squeeze(price.day_of_month(sel_y,sel_m,:,:)));
+	mon_price = transpose(squeeze(price.day_of_month(p_y_idx,cf.m,:,:)));
 else
-	mon_price = cf.telge_avg(sel_y,sel_m)*ones(24,31);
+	mon_price = cf.telge_avg(p_y_idx,cf.m)*ones(24,31);
 end
-mon_trans = transpose(squeeze(cf.transf_price(sel_y,sel_m,weekdays,:)));
+mon_trans = transpose(squeeze(cf.transf_price(p_y_idx,cf.m,weekdays,:)));
 mon_trans(:,num_days_mon+1:end) = NaN;
-tax = cf.eng_tax(sel_y)*ones(24,31);
+tax = cf.eng_tax(p_y_idx)*ones(24,31);
 tax(:,num_days_mon+1:end) = NaN;
 moms = cf.VAT*(tax + mon_trans + mon_price + cf.markup);
 moms(:,num_days_mon+1:end) = NaN;
 
 for u = 1:length(cons.users.ID)
-%for u = sel_u
 	figure()
 	colororder([0 0 0; 1 0 0])
 
@@ -58,14 +52,14 @@ for u = 1:length(cons.users.ID)
 	set(gca,'XLim',[1 24*num_days_mon]);
 
 	yyaxis right;
-	usr_hour = transpose(squeeze(cons_hour(u,sel_y,sel_m,:,:)));
+	usr_hour = transpose(squeeze(cons_hour(u,c_y_idx,cf.m,:,:)));
 	b = bar(hours,usr_hour(:));
 	b.FaceAlpha = .5;
 	ylabel('Laddning per timme [kWh]')
 	set(gca,'XLim',[1 24*num_days_mon]);
 
 	xlabel('Timme i månaden')
-	title(sprintf('Sammanställning för %s under %s (total laddning %.1f kWh)', cons.users.FirstName{u}, cf.month_l_se(sel_m,:), sum(cons.day_of_week(u,sel_y,sel_m,:,:,:),[2,3,4,5,6])));
+	title(sprintf('Sammanställning för %s under %s %d (total laddning %.1f kWh)', cons.users.FirstName{u}, cf.month_l_se(cf.m,:), cf.yr, sum(cons.day_of_week(u,c_y_idx,cf.m,:,:,:),[2,3,4,5,6])));
 	%l.Position(2) = l.Position(2) + .25;
 	if printfigs
 	    set(gcf,'paperunits','centimeters','papersize',papersize,'paperposition',[offs_x,offs_y,papersize(1)-offs_x,papersize(2)-offs_y])
