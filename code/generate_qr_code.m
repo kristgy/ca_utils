d588fa8991b7e2cf1cac6ffb402ec72a025a4948
kristgy <kristgy@users.noreply.github.com>
2023-01-07 19:49:56 +0000

    Playing around with code for SWISH QR code generation.

--- code/generate_qr_code.m
+++ code/generate_qr_code.m
@@ -5,6 +5,10 @@ function qr = generate_qr_code(ocr,due,bg)
 % following the spec from here:
 % https://www.qrkod.info/
 
+% Partial solution for SWISH:
+% sprintf('https://app.swish.nu/1/p/sw/?sw=%s&amt=%1.1f&cur=SEK&msg=%d',bg,due,ocr);
+% according to: https://developer.swish.nu/documentation/guides/generate-qr-codes
+
 %'{ "uqr": 1, "tp": 1, "nme": "Test AB", "cid": "1234", "iref": "1001", "idt": "20220926", "ddt": "20221026", "due": "50", "pt": "BG", "acc": "5536-7742" }'
 message = ...
 sprintf('{ "uqr": 1, "tp": 1, "iref": "%d", "due": "%1.2f", "pt": "BG", "acc": "%s" }',ocr,due,bg);
